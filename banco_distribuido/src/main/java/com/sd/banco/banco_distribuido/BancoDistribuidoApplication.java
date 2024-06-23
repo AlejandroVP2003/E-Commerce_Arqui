93e8fa2ffb5bac84c0def0c5c1d8c9c52e3532db
AlejandroVP2003 <alejandrovalp2003@outlook.com.pe>
2024-06-22 19:10:50 -0500

    Add files via upload

---
+++ banco_distribuido/src/main/java/com/sd/banco/banco_distribuido/BancoDistribuidoApplication.java
@@ -0,0 +1,13 @@
+package com.sd.banco.banco_distribuido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDistribuidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDistribuidoApplication.class, args);
+	}
+
+}
---
+++ banco_distribuido/src/test/java/com/sd/banco/banco_distribuido/BancoDistribuidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd.banco.banco_distribuido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDistribuidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
